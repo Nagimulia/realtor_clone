4a359d96d314a55879d3e9f1c22e387fb7468e7e
Nagimulia <keneuova@mail.ru>
2022-10-10 19:21:40 +0600

    create delete function

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,14 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { collection, doc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { FcHome } from 'react-icons/fc';
 import { Link, useNavigate } from 'react-router-dom';
@@ -70,6 +79,19 @@ const Profile = () => {
     fetchUserListings();
   }, [auth.currentUser.uid]);
 
+  async function onDelete(listingID) {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', listingID));
+      const updatedListings = listings.filter((listing) => listing.id !== listingID);
+      setListings(updatedListings);
+      toast.success('Successfully deleted the listing');
+    }
+  }
+  
+  const onEdit = (listingID) => {
+    navigate(`/edit-listing/${listingID}`);
+  };
+
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -129,7 +151,13 @@ const Profile = () => {
             <h2 className="text-2xl text-center font-semibold mb-6">My Listings</h2>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6">
               {listings.map((listing) => (
-                <ListingItem key={listing.id} id={listing.id} listing={listing.data} />
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                />
               ))}
             </ul>
           </>
